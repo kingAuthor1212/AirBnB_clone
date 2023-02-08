8f23d0cdc77da45769ccbb7a44999842e981c916
kingAuthro1212 <kingauthor1212@gmail.com>
2023-02-08 18:12:01 +0200

    AirBnB_clone

--- console.py
+++ console.py
@@ -9,4 +9,8 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review  import Review
 
-class_list = { "BaseModel": BaseModel, "User": State}
+class_list = { "BaseModel": BaseModel, "User": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+white_list = []
+for key in class_list:
+    white_list.append(key)
+commands = ["do_show", "do_destroy", "do_all"]
