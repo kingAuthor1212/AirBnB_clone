04dd0d7c93e00e497af4d34433280080517ac69d
kingAuthro1212 <kingauthor1212@gmail.com>
2023-02-08 11:09:12 +0200

    clone airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,7 @@ import os.path
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.city import State
+from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
@@ -35,11 +35,21 @@ class FileStorage():
         self.__objects.update({key:obj})
 
     def save(self):
+        """serializes '__objects' to the Json file (__file_path)"""
+        dict = {}
         for key in self.__objects:
             dict[key] = self.__objects[key].to_dict()
         with open(self.__file_path, "w") as f:
             json.dump(dict, f)
 
     def reload(self):
-        
+        """deserializes the Json file to '__objects' (only if the Json file exists, otherwise do nothing. if the file !== exists, no exception should be raised)"""
+        try:
+            with open(self.__file_path, 'r')
+            as f:
+                dict = json.load(f)
+            for i in dict:
+                self.__objects[i] = class_list[dict[i]["__class__"]](**dict[i])
+        expect:
+            pass
     
